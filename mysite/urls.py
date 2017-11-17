c7eb310623435a887640c4f1e5bc0fb9cb1f37bc
newlife <shfeng819@126.com>
2017-11-17 11:34:10 +0800

    add new content

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,7 +1,9 @@
 from django.urls import include, path
 from django.contrib import admin
+from polls.views import index
 
 urlpatterns = [
+    path('index/',index),
     path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
 ]
